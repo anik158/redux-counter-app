1c7b4f86c42f89b427bdab3e9f3bc5f24eeb82ab
Ahsan <ahsanan@outlook.com>
2026-02-19 23:36:30 +0600

    simple implementation of store/action/reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+
+  const count = useSelector(state => state.count);
+
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Redux Counter</h1>
+      <h2>Count : {count}</h2>
+
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
---
+++ src/actions.js
@@ -0,0 +1,8 @@
+// Action creators
+export const increment = () => ({
+    type: 'INCREMENT'
+});
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+});
